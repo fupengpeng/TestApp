9695c6f0aa8958641003be03c9d6f908541d0676
fupengpeng <564055710@qq.com>
2018-01-09 17:31:35 +0800

    progressbar布局搭建。

---
+++ androidtestapp/src/main/java/com/fpp/androidtestapp/activity/impl/progressbar/PargressBarActivity.java
@@ -0,0 +1,21 @@
+package com.fpp.androidtestapp.activity.impl.progressbar;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fpp.androidtestapp.R;
+
+/**
+ * @description   progressbar及子类应用
+ * @author  fupengpeng
+ * @date  2018/1/9 0009 17:18
+ */
+public class PargressBarActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pargress_bar);
+    }
+}
