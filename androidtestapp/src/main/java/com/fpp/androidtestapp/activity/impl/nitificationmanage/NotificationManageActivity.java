a24356fcec0cd44336d349f5053c6f2c32e01352
564055710@qq.com <564055710@qq.com>
2018-03-27 17:27:24 +0800

    状态栏标识颜色变化完善。

--- androidtestapp/src/main/java/com/fpp/androidtestapp/activity/impl/nitificationmanage/NotificationManageActivity.java
+++ androidtestapp/src/main/java/com/fpp/androidtestapp/activity/impl/nitificationmanage/NotificationManageActivity.java
@@ -205,7 +205,16 @@ public class NotificationManageActivity extends AppCompatActivity {
                 getWindow().clearFlags(WindowManager.LayoutParams.TYPE_STATUS_BAR);
                 break;
             case R.id.tv_atvt_notification_hide_fourteen:
-
+                //布局填充状态栏，设置透明
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    decorView = getWindow().getDecorView();
+                    option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+                    decorView.setSystemUiVisibility(option);
+                    getWindow().setStatusBarColor(Color.TRANSPARENT);//透明状态栏
+//                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+                }
 
                 break;
             case R.id.tv_atvt_notification_hide_fifteen:
