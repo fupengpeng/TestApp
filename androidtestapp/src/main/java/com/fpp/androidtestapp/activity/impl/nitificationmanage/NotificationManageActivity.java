0647ee46d8d119d83810658488fd9b9b4472946b
564055710@qq.com <564055710@qq.com>
2018-03-29 17:21:03 +0800

    WindowManager.LayoutParams类更多的静态常量了解。

--- androidtestapp/src/main/java/com/fpp/androidtestapp/activity/impl/nitificationmanage/NotificationManageActivity.java
+++ androidtestapp/src/main/java/com/fpp/androidtestapp/activity/impl/nitificationmanage/NotificationManageActivity.java
@@ -205,28 +205,72 @@ public class NotificationManageActivity extends AppCompatActivity {
                 getWindow().clearFlags(WindowManager.LayoutParams.TYPE_STATUS_BAR);
                 break;
             case R.id.tv_atvt_notification_hide_fourteen:
-                //布局填充状态栏，设置透明
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    decorView = getWindow().getDecorView();
-                    option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
-                    decorView.setSystemUiVisibility(option);
-                    getWindow().setStatusBarColor(Color.TRANSPARENT);//透明状态栏
-//                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-                }
+//                //布局填充状态栏，设置透明
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                    decorView = getWindow().getDecorView();
+//                    option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                            | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+//                    decorView.setSystemUiVisibility(option);
+//                    getWindow().setStatusBarColor(Color.TRANSPARENT);//透明状态栏
+////                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+//                }
 
+//                // 透明状态栏，标题栏
+//                getWindow().clearFlags(WindowManager.LayoutParams.TYPE_INPUT_METHOD_DIALOG);
+//                getWindow().clearFlags(WindowManager.LayoutParams.TYPE_STATUS_BAR_PANEL);
+//                getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+//                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+//                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR);
+
+//                // 隐藏标题栏
+//                getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+//                // 不透明状态栏，标题栏
+//                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+//                getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+//                getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR);
+
+
+//                // 显示标题栏
+//                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//                // 透明导航栏
+//                getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+//                // 状态栏变黑
+//                getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+                getWindow().addFlags(WindowManager.LayoutParams.SOFT_INPUT_STATE_UNCHANGED);
                 break;
             case R.id.tv_atvt_notification_hide_fifteen:
+//                // 恢复原色导航栏
+//                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+//                // 状态栏变黑后恢复原色
+//                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
 
+                getWindow().clearFlags(WindowManager.LayoutParams.SOFT_INPUT_STATE_UNCHANGED);
                 break;
             case R.id.tv_atvt_notification_hide_sixteen:
+//                // 使所有控件变黑
+//                getWindow().addFlags(WindowManager.LayoutParams.FLAG_SCALED);
+                getWindow().addFlags(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
                 break;
             case R.id.tv_atvt_notification_hide_seventeen:
+//                // 使所有控件恢复本色
+//                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SCALED);
+                getWindow().clearFlags(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
                 break;
             case R.id.tv_atvt_notification_hide_eighteen:
+
+                getWindow().addFlags(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+//                // 状态栏变灰
+//                getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
                 break;
             case R.id.tv_atvt_notification_hide_nineteen:
+
+
+                getWindow().clearFlags(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+//                // 状态栏从灰恢复原色
+//                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
                 break;
 
 
